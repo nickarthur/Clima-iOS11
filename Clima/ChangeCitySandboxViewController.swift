46f6c378aa2a2e2d058440cea6e45c4e2ec7ddbc
Larry Nickerson <lnickers@austin.rr.com>
2017-12-03 21:41:54 -0600

    finished re-creating the ui

--- Clima/ChangeCitySandboxViewController.swift
+++ Clima/ChangeCitySandboxViewController.swift
@@ -21,7 +21,37 @@ class ChangeCitySandboxViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
+    //Declare the delegate variable here:
+    
+    
+    @IBOutlet weak var changeCityTextField: UITextField!
+    
+    
+    
+    //This is the IBAction that gets called when the user taps on the "Get Weather" button:
+    @IBAction func getWeatherPressed(_ sender: AnyObject) {
+        
+        
+        
+        //1 Get the city name the user entered in the text field
+        
+        
+        //2 If we have a delegate set, call the method userEnteredANewCityName
+        
+        
+        //3 dismiss the Change City View Controller to go back to the WeatherViewController
+        
+        
+    }
+    
+    
+    
+    //This is the IBAction that gets called when the user taps the back button. It dismisses the ChangeCityViewController.
+    @IBAction func backButtonPressed(_ sender: AnyObject) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    
     /*
     // MARK: - Navigation
 
--- Clima/WeatherSandboxViewController.swift
+++ Clima/WeatherSandboxViewController.swift
@@ -10,10 +10,32 @@ import UIKit
 
 class WeatherSandboxViewController: UIViewController {
 
+    
+    
+    //Constants
+    let WEATHER_URL = "http://api.openweathermap.org/data/2.5/weather"
+    let APP_ID = "ab024415e180044f32f6646a3a987469"
+    
+    
+    //TODO: Declare instance variables here
+    
+    
+    
+    // IBOutlets
+    @IBOutlet weak var weatherIcon: UIImageView!
+    
+    @IBOutlet weak var cityLabel: UILabel!
+    
+    @IBOutlet weak var temperatureLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        //TODO:Set up the location manager here.
+
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,15 +43,73 @@ class WeatherSandboxViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
+    
+    //MARK: - Networking
+    /***************************************************************/
+    
+    //Write the getWeatherData method here:
+    
+    
+    
+    
+    
+    
+    
+    //MARK: - JSON Parsing
+    /***************************************************************/
+    
+    
+    //Write the updateWeatherData method here:
+    
+    
+    
+    
+    
+    //MARK: - UI Updates
+    /***************************************************************/
+    
+    
+    //Write the updateUIWithWeatherData method here:
+    
+    
+    
+    
+    
+    
+    //MARK: - Location Manager Delegate Methods
+    /***************************************************************/
+    
+    
+    //Write the didUpdateLocations method here:
+    
+    
+    
+    //Write the didFailWithError method here:
+    
+    
+    
+    
+    
+    //MARK: - Change City Delegate methods
+    /***************************************************************/
+    
+    
+    //Write the userEnteredANewCityName Delegate method here:
+    
+    
+    
+    //Write the PrepareForSegue Method here
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
+    
 
 }
